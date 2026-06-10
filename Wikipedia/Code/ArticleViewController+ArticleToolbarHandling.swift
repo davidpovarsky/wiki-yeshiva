0d991b05ed405ca26cc93520d36e295d01dc84d0
davidpovarsky <davidpovarski1@gmail.com>
2026-06-10 20:11:57 +0300

    Add article wiki source picker handling

--- Wikipedia/Code/ArticleViewController+ArticleToolbarHandling.swift
+++ Wikipedia/Code/ArticleViewController+ArticleToolbarHandling.swift
@@ -1,3 +1,4 @@
+import UIKit
 import WMFComponents
 import WMFData
 
@@ -81,6 +82,10 @@ extension ArticleViewController: ArticleToolbarHandling {
         NavigationEventsFunnel.shared.logEvent(action: .articleToolbarLang)
         showLanguages()
     }
+
+    func showWikiSourcePicker(from controller: ArticleToolbarController) {
+        presentWikiSourcePicker(from: controller)
+    }
     
     func share(from controller: ArticleToolbarController) {
         NavigationEventsFunnel.shared.logEvent(action: .articleToolbarShare)
@@ -112,4 +117,120 @@ extension ArticleViewController: ArticleToolbarHandling {
         showEditorForFullSource()
     }
     
+}
+
+private extension ArticleViewController {
+    func presentWikiSourcePicker(from controller: ArticleToolbarController) {
+        guard let currentTitle = articleURL.wmf_title, !currentTitle.isEmpty else {
+            let alert = UIAlertController(title: "Choose Wiki", message: "This page does not have a title that can be opened on another wiki.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: CommonStrings.okTitle, style: .default))
+            present(alert, animated: true)
+            return
+        }
+
+        let languageCode = articleURL.wmf_languageCode ?? dataStore.languageLinkController.appLanguage?.languageCode ?? "he"
+        let sources = WikiReadingSource.defaultSources(languageCode: languageCode)
+        let alert = UIAlertController(title: "Choose Wiki", message: "Open this title on another wiki source.", preferredStyle: .actionSheet)
+
+        for source in sources {
+            let title = source.matches(articleURL) ? "✓ \(source.displayName)" : source.displayName
+            alert.addAction(UIAlertAction(title: title, style: .default, handler: { [weak self] _ in
+                self?.open(currentTitle: currentTitle, in: source, languageCode: languageCode)
+            }))
+        }
+
+        alert.addAction(UIAlertAction(title: CommonStrings.cancelActionTitle, style: .cancel))
+
+        if let popover = alert.popoverPresentationController {
+            popover.barButtonItem = controller.moreButton
+            popover.sourceView = view
+            popover.sourceRect = view.bounds
+        }
+
+        present(alert, animated: true)
+    }
+
+    func open(currentTitle: String, in source: WikiReadingSource, languageCode: String) {
+        guard let navigationController,
+              let newURL = source.articleURL(for: currentTitle, languageCode: languageCode) else {
+            return
+        }
+
+        guard newURL != articleURL else {
+            return
+        }
+
+        let articleCoordinator = ArticleCoordinator(
+            navigationController: navigationController,
+            articleURL: newURL,
+            dataStore: dataStore,
+            theme: theme,
+            needsAnimation: true,
+            source: .undefined,
+            tabConfig: .appendArticleAndAssignCurrentTabAndCleanoutFutureArticles
+        )
+        articleCoordinator.start()
+    }
+}
+
+private struct WikiReadingSource {
+    enum PathStyle {
+        case languageSubdomain(rootDomain: String)
+        case fixedHost(host: String, articlePathPrefix: String)
+    }
+
+    let displayName: String
+    let style: PathStyle
+
+    static func defaultSources(languageCode: String) -> [WikiReadingSource] {
+        return [
+            WikiReadingSource(displayName: "Wikipedia", style: .languageSubdomain(rootDomain: "wikipedia.org")),
+            WikiReadingSource(displayName: "Wiktionary", style: .languageSubdomain(rootDomain: "wiktionary.org")),
+            WikiReadingSource(displayName: "Wikisource", style: .languageSubdomain(rootDomain: "wikisource.org")),
+            WikiReadingSource(displayName: "Wikiquote", style: .languageSubdomain(rootDomain: "wikiquote.org")),
+            WikiReadingSource(displayName: "Wikibooks", style: .languageSubdomain(rootDomain: "wikibooks.org")),
+            WikiReadingSource(displayName: "Wikiversity", style: .languageSubdomain(rootDomain: "wikiversity.org")),
+            WikiReadingSource(displayName: "Wikinews", style: .languageSubdomain(rootDomain: "wikinews.org")),
+            WikiReadingSource(displayName: "Wikivoyage", style: .languageSubdomain(rootDomain: "wikivoyage.org")),
+            WikiReadingSource(displayName: "WikiYeshiva", style: .fixedHost(host: "www.yeshiva.org.il", articlePathPrefix: "/wiki/"))
+        ]
+    }
+
+    func matches(_ url: URL) -> Bool {
+        guard let host = url.host?.lowercased() else {
+            return false
+        }
+
+        switch style {
+        case .languageSubdomain(let rootDomain):
+            return host == rootDomain || host.hasSuffix(".\(rootDomain)")
+        case .fixedHost(let fixedHost, _):
+            return host == fixedHost.lowercased()
+        }
+    }
+
+    func articleURL(for title: String, languageCode: String) -> URL? {
+        let normalizedLanguageCode = languageCode.isEmpty ? "he" : languageCode
+        let normalizedTitle = title.replacingOccurrences(of: " ", with: "_")
+        var allowedCharacters = CharacterSet.urlPathAllowed
+        allowedCharacters.remove(charactersIn: "/")
+
+        guard let encodedTitle = normalizedTitle.addingPercentEncoding(withAllowedCharacters: allowedCharacters) else {
+            return nil
+        }
+
+        var components = URLComponents()
+        components.scheme = "https"
+
+        switch style {
+        case .languageSubdomain(let rootDomain):
+            components.host = "\(normalizedLanguageCode).\(rootDomain)"
+            components.path = "/wiki/\(encodedTitle)"
+        case .fixedHost(let host, let articlePathPrefix):
+            components.host = host
+            components.path = "\(articlePathPrefix)\(encodedTitle)"
+        }
+
+        return components.url
+    }
 }
